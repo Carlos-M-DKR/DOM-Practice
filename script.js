6027729ba6789dc1edde7907eca25cf0375686ac
Carlos Martinez <carlosimportant@gmail.com>
2025-06-17 20:38:10 +0000

    added form

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("greeting").textContent = "hi there!";
+document.getElementById("H1").textContent = "A Heading";
+
+/*The Heading*/
+
+/*
+run this first: php -5 localhost: 3006 -t ./
+*/
