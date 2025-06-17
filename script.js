114c26848d1c8f650d8ed3db95ee63c54e0f8552
Carlos Martinez <carlosimportant@gmail.com>
2025-06-17 21:31:13 +0000

    fhthe

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 document.getElementById("greeting").textContent = "hi there!";
-document.getElementById("H1").textContent = "A Heading";
+document.getElementById("h1").textContent = "The Header";
 
-/*The Heading*/
+let items = document.getElementsByClassName("redclass");
+for (let i = 0; i < items.length; i++)
+{
+    items[i].textContent = "Text change";
+}
+
+const header = document.querySelector("h1");
+console.log(header);
 
 /*
 run this first: php -5 localhost: 3006 -t ./
