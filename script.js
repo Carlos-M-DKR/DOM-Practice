5a26b5504ee060a451693e8a53f3c6176f40b07e
Carlos Martinez <carlosimportant@gmail.com>
2025-06-18 02:49:46 +0000

    added form

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ for (let i = 0; i < items.length; i++)
 
 const header = document.querySelector("h1");
 console.log(header);
+const red = document.querySelectorAll(".redclass");
+console.log(red);
+document.getElementById("click").addEventListener("click", function()
+{
+    alert("CLICK BUTTON!");
+});
 
 /*
 run this first: php -5 localhost: 3006 -t ./
